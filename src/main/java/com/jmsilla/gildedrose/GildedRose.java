06b4c8d2b6f360bdd6a04c307927448da6740ea1
JMSilla <josilma1@gmail.com>
2019-11-24 11:20:19 +0000

    First refactor iteration.

---
+++ src/main/java/com/jmsilla/gildedrose/GildedRose.java
@@ -0,0 +1,89 @@
+package com.jmsilla.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    private static class ItemNames {
+        private static final String AGED_BRIE = "Aged Brie";
+        private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+        private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    }
+    
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items)
+            updateItem(item);
+    }
+
+    private void updateItem(Item item) {
+        switch(item.name) {
+        case ItemNames.AGED_BRIE:
+            incrementItemQuality(item);
+            decrementItemSellIn(item);
+            
+            if (sellInDayHasPassed(item))
+                incrementItemQuality(item);
+            
+            break;
+            
+        case ItemNames.BACKSTAGE:
+            incrementItemQuality(item);
+            
+            if (daysToItemSellAreLessThan(item, 11))
+                incrementItemQuality(item);
+            
+            if (daysToItemSellAreLessThan(item, 6))
+                incrementItemQuality(item);
+            
+            decrementItemSellIn(item);
+            
+            if (sellInDayHasPassed(item))
+                removeItemQuality(item);
+            
+            break;
+            
+        case ItemNames.SULFURAS:
+            break;
+        
+        default:
+            decrementItemQuality(item);
+            
+            decrementItemSellIn(item);
+            
+            if (sellInDayHasPassed(item))
+                decrementItemQuality(item);
+        }
+    }
+
+    private void incrementItemQuality(Item item) {
+        if (item.quality < MAX_QUALITY)
+            item.quality = item.quality + 1;
+    }
+
+    private void decrementItemSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private boolean sellInDayHasPassed(Item item) {
+        return item.sellIn < 0;
+    }
+    
+    private boolean daysToItemSellAreLessThan(Item item, int days) {
+        return item.sellIn < days;
+    }
+
+    private void removeItemQuality(Item item) {
+        item.quality = MIN_QUALITY;
+    }
+
+    private void decrementItemQuality(Item item) {
+        if (item.quality > MIN_QUALITY)
+            item.quality = item.quality - 1;
+    }
+}
